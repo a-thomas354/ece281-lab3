295549a66d5c60ce839bb8645b25057158b1b664
a-thomas-354 <142030204+a-thomas354@users.noreply.github.com>
2024-03-31 21:43:43 -0600

    Finished Lab 3

--- README.md
+++ README.md
@@ -26,3 +26,5 @@ env:
 
 If successful then GHDL will quietly exit with a `0` code.
 If any of the `assert` statements fail **with** `severity error` then GHDL will cease the simulation and exit with non-zero code; this will also cause the workflow to fail.
+
+Documention: C3C Malani helpped me understand the initalization of my w_thunderbird in my tb file, the logic behind making the clock run on 4 Hz, and gave me advice on how to make more in depth case test which included testing for the completion of a sequence even after the switch is off.
--- src/hdl/thunderbird_fsm_tb.vhd
+++ src/hdl/thunderbird_fsm_tb.vhd
@@ -58,27 +58,104 @@ architecture test_bench of thunderbird_fsm_tb is
 	
 	component thunderbird_fsm is 
 	  port(
-		
+	  i_clk, i_reset  : in    std_logic;
+      i_left, i_right : in    std_logic;
+      o_lights_L      : out   std_logic_vector(2 downto 0);
+      o_lights_R      : out   std_logic_vector(2 downto 0)	
 	  );
 	end component thunderbird_fsm;
 
 	-- test I/O signals
-	
+	signal w_L : std_logic := '0';
+	signal w_R : std_logic := '0';
+    signal w_reset : std_logic := '0';
+    signal w_clk : std_logic := '0';
 	-- constants
-	
+	signal w_thunderbird : std_logic_vector(5 downto 0) := "000000"; --  one-hot
+        
+    -- Clock period definitions
+    constant k_clk_period : time := 10 ns;
 	
 begin
 	-- PORT MAPS ----------------------------------------
-	
+	uut: thunderbird_fsm port map(
+	i_clk => w_clk,
+	i_reset => w_reset,
+	i_left => w_L,
+	i_right => w_R,
+	o_lights_L(0) => w_thunderbird(3),
+    o_lights_L(1) => w_thunderbird(4),
+    o_lights_L(2) => w_thunderbird(5),
+    o_lights_R(0) => w_thunderbird(2),
+    o_lights_R(1) => w_thunderbird(1),
+    o_lights_R(2) => w_thunderbird(0)
+	);
 	-----------------------------------------------------
 	
 	-- PROCESSES ----------------------------------------	
     -- Clock process ------------------------------------
-    
+    	clk_proc : process
+    begin
+        w_clk <= '0';
+        wait for k_clk_period/2;
+        w_clk <= '1';
+        wait for k_clk_period/2;
+    end process;
 	-----------------------------------------------------
-	
+	sim_proc: process
+	begin
 	-- Test Plan Process --------------------------------
-	
+	w_reset <= '1'; --TEST RESET
+	wait for k_clk_period*1;
+    assert w_thunderbird = "000000" report "bad reset" severity failure;
+    
+    w_reset <= '0';
+    wait for k_clk_period*1;
+    
+   w_L <= '1'; w_R <= '1' ; wait for k_clk_period; --TEST HAZARD
+   assert	w_thunderbird = "111111" report "bad hazard" severity failure;
+   wait for k_clk_period;
+   assert w_thunderbird = "000000" report "bad hazard" severity failure;
+  
+    w_R <= '0'; w_L <= '0'; wait for k_clk_period*1;
+    assert w_thunderbird = "000000" report "all lights need to be off" severity failure;
+    
+    w_R <= '0'; w_L <= '1'; wait for k_clk_period*1; 
+    assert w_thunderbird = "001000" report "first right light" severity failure;
+    wait for k_clk_period;
+    assert w_thunderbird = "011000" report "second right light" severity failure;
+    wait for k_clk_period;
+    assert w_thunderbird = "111000" report "third right light" severity failure;
+    wait for k_clk_period;
+    assert w_thunderbird = "000000" report "reset right light" severity failure;
+    
+    w_R <= '0'; w_L <= '1'; wait for k_clk_period*1; 
+    assert w_thunderbird = "001000" report "first right light" severity failure;
+    w_R <= '0'; w_L <= '0'; wait for k_clk_period*1;
+    assert w_thunderbird = "011000" report "second right light" severity failure;
+    wait for k_clk_period;
+    assert w_thunderbird = "111000" report "third right light" severity failure;
+    wait for k_clk_period;
+    assert w_thunderbird = "000000" report "reset right light" severity failure;
+    
+    w_R <= '1'; w_L <= '0'; wait for k_clk_period;
+     assert w_thunderbird = "000100" report "first left light" severity failure;
+    wait for k_clk_period;
+    assert w_thunderbird = "000110" report "second left light" severity failure;
+    wait for k_clk_period;
+    assert w_thunderbird = "000111" report "third left light" severity failure;
+    wait for k_clk_period;
+    assert w_thunderbird = "000000" report "reset left light" severity failure;
+  
+     w_R <= '1'; w_L <= '0'; wait for k_clk_period;
+       assert w_thunderbird = "000100" report "first left light" severity failure;
+     w_R <= '0'; w_L <= '0'; wait for k_clk_period;
+      assert w_thunderbird = "000110" report "second left light" severity failure;
+      wait for k_clk_period;
+      assert w_thunderbird = "000111" report "third left light" severity failure;
+      wait for k_clk_period;
+      assert w_thunderbird = "000000" report "reset left light" severity failure;
 	-----------------------------------------------------	
-	
+	wait;
+	end process;
 end test_bench;
